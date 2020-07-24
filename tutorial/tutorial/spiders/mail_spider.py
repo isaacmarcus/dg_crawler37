d8b99bffa055fa39698a1bfbeaf6f84040b9c22a
Isaac Marcus <isaacmarcus@users.noreply.github.com>
2020-07-24 17:59:11 +0800

    added functionality of crawling phone numbers, added validation of emails, and validation of phone numbers

--- tutorial/tutorial/spiders/mail_spider.py
+++ tutorial/tutorial/spiders/mail_spider.py
@@ -8,7 +8,8 @@ from lxml.html import fromstring
 from googlesearch import search
 from scrapy.crawler import CrawlerProcess
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
-from validate_email import validate_email
+from email_validator import validate_email, EmailNotValidError
+import phonenumbers as pn
 
 
 class MailSpider(scrapy.Spider):
@@ -43,26 +44,50 @@ class MailSpider(scrapy.Spider):
             html_text = str(response.text)
         except Exception as e:
             print(e)
+        # regex parameters for searching for emails
+        phone_list = re.findall('(?:\+65 [\d]{4} [\d]{4}|\+65[\d]{8}|[\d]{4} [\d]{4}||[\d]{8})+', html_text)
         mail_list = re.findall('(?!.*\w{1}\.\w{1}\.\w{1})(?!.*\.google\.com)(?:(\w+@\w+\.[\w+\.{1}]+))+', html_text)
-
+        # empty lists to add valid email and phone numbers to
+        newPhoneList = []
+        newMailList = []
         # only loop through is mail list is not empty
         if len(mail_list) > 0:
             for i in range(len(mail_list)):
                 # change mail item to lowercase
                 mail_list[i] = mail_list[i].lower()
                 # loop through reject words list
+                reject_found = False
                 for word in reject_words_regex:
                     # if word is found in email, remove it and all instances of it from list
                     if str(word.lower()) in str(mail_list[i].lower()):
-                        mail_list = list(filter(mail_list[i].__ne__, mail_list))
+                        reject_found = True
+                        # mail_list = list(filter(mail_list[i].__ne__, mail_list))
                 # double check if valid email format
-                if not validate_email(mail_list[i]):
-                    # if not valid, remove all instances
-                    mail_list = list(filter(mail_list[i].__ne__, mail_list))
+                try:
+                    validEmail = validate_email(mail_list[i])
+                    mail_list[i] = validEmail.ascii_email
+                    # append to new list if no reject word found, not already in list and is a valid email
+                    if not reject_found and mail_list[i] not in newMailList:
+                        newMailList.append(validEmail.ascii_email)
+                except Exception as e:
+                    print(e)
+                    # if not valid, don't add to new list
+                    # mail_list = list(filter(mail_list[i].__ne__, mail_list))
 
-        dic = {'email': mail_list, 'link': str(response.url)}
-        df = pd.DataFrame(dic)
-        df.to_csv(self.path, mode='a', header=False)
+        # check if phone list is empty
+        if len(phone_list) > 0:
+            for numberIndex in range(len(phone_list)):
+                parsedNumber = pn.parse(phone_list[numberIndex], region='SG')
+                if pn.is_valid_number(parsedNumber) and phone_list[numberIndex] not in newPhoneList:
+                    newPhoneList.append(phone_list[numberIndex])
+
+        # add dictiionary of emails and numbers to csv
+        phonedic = {'phone': newPhoneList, 'link': str(response.url)}
+        maildic = {'email': newMailList, 'link': str(response.url)}
+        maildf = pd.DataFrame(maildic)
+        maildf.to_csv("mail_" + self.path, mode='a', header=False)
+        phonedf = pd.DataFrame(phonedic)
+        phonedf.to_csv("ph_" + self.path, mode='a', header=False)
 
 
 def get_proxies():
@@ -108,9 +133,11 @@ def create_file(path):
 
 
 def get_info(tag, n, language, path, reject=[]):
-    create_file(path)
+    create_file("mail_" + path)
     df = pd.DataFrame(columns=['email', 'link'], index=[0])
-    df.to_csv(path, mode='w', header=True)
+    df.to_csv("mail_" + path, mode='w', header=True)
+    phdf = pd.DataFrame(columns=['phone', 'link'], index=[0])
+    phdf.to_csv("ph_" + path, mode='w', header=True)
 
     print('Collecting proxies...')
     get_proxies()
@@ -125,11 +152,17 @@ def get_info(tag, n, language, path, reject=[]):
     process.start()
 
     print('Cleaning emails...')
-    df = pd.read_csv(path, index_col=0)
+    df = pd.read_csv("mail_" + path, index_col=0)
     df.columns = ['email', 'link']
     df = df.drop_duplicates(subset='email')
     df = df.reset_index(drop=True)
-    df.to_csv(path, mode='w', header=True)
+    df.to_csv("mail_" + path, mode='w', header=True)
+
+    phdf = pd.read_csv("ph_" + path, index_col=0)
+    phdf.columns = ['phone', 'link']
+    phdf = df.drop_duplicates(subset='phone')
+    phdf = df.reset_index(drop=True)
+    phdf.to_csv("ph_" + path, mode='w', header=True)
 
     process.stop()
 
@@ -146,14 +179,16 @@ def file_namer(search_term):
 
 
 key = "AIzaSyAv8Cbe7A-tlSfEwLDwo7romFPRj7vXD2Y"
-search_param = "Chemicals Industry Contacts"
+search_param = "Singapore Pharmaceuticals Contact"
+# Incomplete: Chemicals Industry Contacts
 csv_path = "paints_coatings.csv"
 reject_words = ["facebook", "wikipedia", "twitter", 'wiki', 'youtube', "flickr", "instagram", "imgur", "jobscentral",
                 "jobstreet", "linkedin", "straitstimes", "channelnewsasia", "sphsubscription", "todayonline", "asiaone",
-                "singaporenews", "mothership", "theindependent", "career", "careers", "pdf"]
-search_terms = ["Singapore Pharmaceuticals Contact", "Aerospace Contact", "Paints and Coatings Contact", "Chemicals Industry Contact"]
+                "singaporenews", "mothership", "theindependent", "career", "careers", "pdf", "shell", "taleo"]
+search_terms = ["Singapore Pharmaceuticals Contact", "Aerospace Contact", "Paints and Coatings Contact", "Chemicals Industry Contact",
+                "Optical Coatings Contacts", "Medical Apparatus Manufacturer Contact"]
 
-get_info(search_param, 100, 'en', file_namer(search_param), reject=reject_words)
+get_info(search_param, 50, 'en', file_namer(search_param), reject=reject_words)
 
 # df = pd.read_csv(csv_path, index_col=0)
 # df.columns = ['email', 'link']
--- tutorial/tutorial/spiders/regex_tester.py
+++ tutorial/tutorial/spiders/regex_tester.py
@@ -1,7 +1,27 @@
 import re
+import phonenumbers as pn
+import pandas as pd
 
-html_text = 'info@clariant.com \n info@infocentric.research.com \n icons@2.4.1 \n service@hispaint.com \n bla@calendar.google.com'
+html_text = 'info@clariant.com \n info@infocentric.research.com \n icons@2.4.1 \n service@hispaint.com \n bla@calendar.google.com' \
+            '\n +65 9678 1723 \n +6598769876 \n 9876 5432 \n 1234 5678 \n 12345678 \n 87654321 \n 98765432 \n 6598765433'
 mail_list = re.findall('(?!.*\w{1}\.\w{1}\.\w{1})(?!.*\.google\.com)(?:(\w+@\w+\.[\w+\.{1}]+))+', html_text)
+
+phone_list = re.findall('(?:\+65 [\d]{4} [\d]{4}|\+65[\d]{8}|[\d]{4} [\d]{4}|[\d]{8})+', html_text)
 
 for mail in mail_list:
     print(mail)
+
+newPhoneList = []
+
+for numberIndex in range(len(phone_list)):
+    parsedNumber = pn.parse(phone_list[numberIndex], region='SG')
+    if pn.is_valid_number(parsedNumber) and phone_list[numberIndex] not in newPhoneList:
+        newPhoneList.append(phone_list[numberIndex])
+
+loldic = {'phone': phone_list, 'link' : str("this is the url")}
+dicdic = {'email': newPhoneList, 'link': str("this is the url")}
+maildf = pd.DataFrame(dicdic)
+phonedf = pd.DataFrame(loldic)
+print(phonedf)
+newdf = maildf.join(phonedf)
+print(newdf)
